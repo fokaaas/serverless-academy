6935f301bbe18f047f7252ce3e57e62443881036
Stanislav Basarab <stanislavbasarab083@gmail.com>
2023-10-21 09:57:26 +0300

    Added entry point

--- 01_cli_interactive_sort/src/index.js
+++ 01_cli_interactive_sort/src/index.js
@@ -1,4 +1,9 @@
 import { Sorter } from './classes/Sorter.js';
+import { log } from './utils/log.js';
+import { STRINGS } from './utils/STRINGS.js';
 
-const sorter = new Sorter()
-await sorter.start()
+const sorter = new Sorter();
+
+console.clear();
+log.info(STRINGS.intro);
+sorter.start().catch((e) => log.err(e));
