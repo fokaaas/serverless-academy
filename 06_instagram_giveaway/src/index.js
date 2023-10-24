abbc636777e50c3202fb19d4c9885ec3ac62889e
Stanislav Basarab <stanislavbasarab083@gmail.com>
2023-10-24 21:09:52 +0300

    Added timers and improved performance

--- 06_instagram_giveaway/src/index.js
+++ 06_instagram_giveaway/src/index.js
@@ -1,42 +1,55 @@
-import * as fs from 'fs/promises'
+import * as fs from 'fs'
 import path from 'path';
 
 const FILES = 20;
 
+console.time('program..')
+
 const data = [];
-for (let i = 0; i < FILES; i++) {
-  const text = await fs.readFile(path.resolve(`../files/out${i}.txt`), 'utf-8');
-  data.push(text.split('\n'));
+const numbers = {};
+
+const dataProcessing = () => {
+  for (let i = 0; i < FILES; i++) {
+    const text = fs.readFileSync(path.resolve(`../files/out${i}.txt`), 'utf-8')
+    const words = text.split('\n');
+    data.push(...new Set(words));
+  }
+  for (const username of data) {
+    numbers[username] = (numbers[username] || 0) + 1;
+  }
 }
 
-const uniqueValues = () => new Set(data.flat()).size;
+const uniqueValues = () => new Set(data).size;
 
 const existInAllFiles = () => {
-  const unique = data.map((arr) => [...new Set(arr)]).flat();
-  const obj = {};
   let counter = 0;
-  for (const username of unique) {
-    obj[username] = (obj[username] || 0) + 1;
-    if (obj[username] === FILES) {
-      counter++;
-    }
+  for (const username in numbers) {
+    if (numbers[username] === FILES) counter++;
   }
   return counter;
 }
 
 const existInAtLeastTen = () => {
-  const unique = data.map((arr) => [...new Set(arr)]).flat();
-  const obj = {};
   let counter = 0;
-  for (const username of unique) {
-    obj[username] = (obj[username] || 0) + 1;
-  }
-  for (const username in obj) {
-    if (obj[username] >= 10) counter++;
+  for (const username in numbers) {
+    if (numbers[username] >= 10) counter++;
   }
   return counter;
 }
 
+console.time('data processing..')
+dataProcessing()
+console.timeEnd('data processing..')
+
+console.time('unique values..')
 console.log(uniqueValues());
+console.timeEnd('unique values..')
+
+console.time('exist in all files..')
 console.log(existInAllFiles());
+console.timeEnd('exist in all files..')
+
+console.time('exist in at least ten..')
 console.log(existInAtLeastTen());
+console.timeEnd('exist in at least ten..')
+console.timeEnd('program..')
